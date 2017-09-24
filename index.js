da615f9529d201db3d52f557d0dff293997ba99e
Rohan Mazumdar <rohan1443@gmail.com>
2017-09-24 15:26:05 +0530

    init commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
 
-const PORT = 3000 || process.env.PORT
+const PORT = process.env.PORT || 3000 
 
 app.get('/', (req, res) => {
  res.send({'hi': 'there'})
 })
 
-app.listen(PORT, () => console.log('Server Started'))
+app.listen(PORT)
